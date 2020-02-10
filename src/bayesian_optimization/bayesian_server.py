d13d2873781bb7c3b0629fd4fd36e27b41888193
Claudio Coppola <claudiocoppola90@gmail.com>
2020-02-10 20:09:21 +0000

    restoring __init__ and importing __future__

--- src/bayesian_optimization/bayesian_server.py
+++ src/bayesian_optimization/bayesian_server.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
+from __future__ import print_function, division
 
-from bopt_grasp_quality.srv import bopt,Addpose, Addscore
 import rospy
 
+from bopt_grasp_quality.srv import bopt, Addpose, Addscore
+
 def handle_pose(req):
-    print "Returning [%s + %s = %s]"%(req.pose, score)
+    print("Returning [%s + %s = %s]"%(req.pose, score))
     return Addpose(req.pose + req.score)
 
 def handle_score(req):
-    print "Returning [%s + %s = %s]"%(req.pose, score)
+    print("Returning [%s + %s = %s]" % (req.pose, score))
     return Addscore(req.pose - req.score)
 
 def add_two_ints_server():
     rospy.init_node('bayesian_server')
     s = rospy.Service('bayesian_pose', bopt, handle_pose)
     g = rospy.Service('bayesian_score', bopt, handle_score)  
-    print "bayesian output is"
+    print("bayesian output is")
     rospy.spin()
 
 if __name__ == "__main__":
