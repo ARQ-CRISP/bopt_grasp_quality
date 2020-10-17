7f271ea8f76082b1accdeaf1f91c140dc8fd966f
Claudio Coppola <claudiocoppola90@gmail.com>
2020-10-17 17:12:04 +0100

    conditional import

--- src/bayesian_optimization/__init__.py
+++ src/bayesian_optimization/__init__.py
@@ -1,6 +1,15 @@
 #!/bin/python
-from bayesopt_quality import BayesOpt_BO
-from skopt_quality import Skopt_BO
+try:
+    from bayesopt_quality import BayesOpt_BO
+except Exception as e:
+    print('WARNING: BayesOpt library may not be installed')
+    pass
+try:
+    from skopt_quality import Skopt_BO
+except Exception as e:
+    print('WARNING: scikit opt may not be properly installed.')
+    pass
+
 from random_discrete_quality import Random_Explorer
 
 __all__ = ['BayesOpt_BO', 'Skopt_BO', 'Random_Explorer']
